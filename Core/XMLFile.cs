27dce28700f142c68930560f007ec4acda7e2f80
Avner Yanai <yanaiavner@gmail.com>
2015-08-20 15:41:34 -0500

    Add xml eterate

--- Core/XMLFile.cs
+++ Core/XMLFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Xml.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace DataDiscovery.Core
 {
@@ -23,23 +24,9 @@ namespace DataDiscovery.Core
             {
                 ClearElemntColumnsNames();
 
-                foreach (var nodes in doc.Elements())
-                {
-                    foreach (var item in nodes.Elements())
-                    {
-                        if(IsFirstElemntHeader)
-                        {
-                            AddElemntColumnsName(item.Value.ToString());
-                        }
-                        else 
-                        {
-                            AddElemntColumnsName(item.Name.ToString());
-                        }
+                var firstElement = doc.Elements().FirstOrDefault();
 
-                    }
-
-                    break;
-                }
+                EterateXMLElmentColumn(firstElement,1);
             }
             catch (Exception ex)
             {
@@ -50,14 +37,43 @@ namespace DataDiscovery.Core
             return ElemntNames;
         }
 
-        public int AnalyzeElement(XElement doc)
+        private void EterateXMLElmentColumn(XElement doc, int level)
         {
+            if(IsFirstElemntHeader)
+            {
+                AddElemntColumnsName(doc.Value.ToString(),level);
+            }
+            else 
+            {
+                AddElemntColumnsName(doc.Name.ToString(),level);
+            }
+
+            var nextLevel = ++level;
+
             foreach (var item in doc.Elements())
             {
 
+                EterateXMLElmentColumn(item, nextLevel);
             }
+        }
+
+        public int AnalyzeElement(XElement doc)
+        {
+            EterateXMLElment(doc, 1);
 
             return ++AnalyzedElmentsCounter;
+        }
+
+        private void EterateXMLElment(XElement doc, int level)
+        {
+            AddElemntColumnsName(doc.Name.ToString(), level);
+
+            var nextLevel = level++;
+
+            foreach (var item in doc.Elements())
+            {
+                EterateXMLElment(item, nextLevel);
+            }
         }
 
         public async Task<bool>  AnalyzeFileAsync(XElement doc)
@@ -66,8 +82,10 @@ namespace DataDiscovery.Core
             {
                 AnalyzedElmentsCounter = 0;
 
+
                 foreach (var elment in doc.Elements())
                 {
+                    
                     await Task.Run(() => {  AnalyzeElement(elment);});
                 }
             }
